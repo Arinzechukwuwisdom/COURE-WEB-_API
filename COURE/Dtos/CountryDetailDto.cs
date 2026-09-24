9875d354e92e783314b956bc4b25fdac848ba84b
Arinzechukwu Ogbu <arinzechukwuwisdomogbu@gmail.com>
2026-09-24 06:25:34 +0100

    Added Dtos

---
+++ COURE/Dtos/CountryDetailDto.cs
@@ -0,0 +1,9 @@
+﻿namespace COURE.Dtos
+{
+    public class CountryDetailDto
+    {
+        public int Id { get; set; }
+        public string Operator { get; set; }
+        public string OperatorCode { get; set; }
+    }
+}
---
+++ COURE/Dtos/CountryDto.cs
@@ -0,0 +1,12 @@
+﻿namespace COURE.Dtos
+{
+    public class CountryDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string CountryIso { get; set; }
+        public int CountryCode { get; set; }
+
+        public CountryDetailDto? CountryDetail { get; set; }
+    }
+}
